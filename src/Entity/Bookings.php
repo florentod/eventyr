53f9e5ae0c6b3079a0e35502263bfaf1c1ef19dc
Moussaillon <paulviain3@gmail.com>
2021-06-15 11:26:29 +0200

    user_booking_dateprice

--- src/Entity/Bookings.php
+++ src/Entity/Bookings.php
@@ -17,8 +17,25 @@ class Bookings
      */
     private $id;
 
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private $comment_content;
+
     public function getId(): ?int
     {
         return $this->id;
     }
+
+    public function getCommentContent(): ?string
+    {
+        return $this->comment_content;
+    }
+
+    public function setCommentContent(?string $comment_content): self
+    {
+        $this->comment_content = $comment_content;
+
+        return $this;
+    } 
 }
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -41,6 +41,81 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
      */
     private $Username;
 
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $user_civility;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $user_first_name;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $user_last_name;
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $user_birthday;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $user_nationality;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $user_address;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $user_city;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $user_zipcode;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $user_phone;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $user_mobile;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $user_passport_number;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $user_passport_country;
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $user_passport_date;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $user_photo;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $user_select_comment;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -130,6 +205,186 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     {
         $this->Username = $Username;
         
+        return $this;
+    }
+
+    public function getUserCivility(): ?string
+    {
+        return $this->user_civility;
+    }
+
+    public function setUserCivility(string $user_civility): self
+    {
+        $this->user_civility = $user_civility;
+
+        return $this;
+    }
+
+    public function getUserFirstName(): ?string
+    {
+        return $this->user_first_name;
+    }
+
+    public function setUserFirstName(string $user_first_name): self
+    {
+        $this->user_first_name = $user_first_name;
+
+        return $this;
+    }
+
+    public function getUserLastName(): ?string
+    {
+        return $this->user_last_name;
+    }
+
+    public function setUserLastName(string $user_last_name): self
+    {
+        $this->user_last_name = $user_last_name;
+
+        return $this;
+    }
+
+    public function getUserBirthday(): ?\DateTimeInterface
+    {
+        return $this->user_birthday;
+    }
+
+    public function setUserBirthday(\DateTimeInterface $user_birthday): self
+    {
+        $this->user_birthday = $user_birthday;
+
+        return $this;
+    }
+
+    public function getUserNationality(): ?string
+    {
+        return $this->user_nationality;
+    }
+
+    public function setUserNationality(string $user_nationality): self
+    {
+        $this->user_nationality = $user_nationality;
+
+        return $this;
+    }
+
+    public function getUserAddress(): ?string
+    {
+        return $this->user_address;
+    }
+
+    public function setUserAddress(string $user_address): self
+    {
+        $this->user_address = $user_address;
+
+        return $this;
+    }
+
+    public function getUserCity(): ?string
+    {
+        return $this->user_city;
+    }
+
+    public function setUserCity(string $user_city): self
+    {
+        $this->user_city = $user_city;
+
+        return $this;
+    }
+
+    public function getUserZipcode(): ?string
+    {
+        return $this->user_zipcode;
+    }
+
+    public function setUserZipcode(string $user_zipcode): self
+    {
+        $this->user_zipcode = $user_zipcode;
+
+        return $this;
+    }
+
+    public function getUserPhone(): ?int
+    {
+        return $this->user_phone;
+    }
+
+    public function setUserPhone(int $user_phone): self
+    {
+        $this->user_phone = $user_phone;
+
+        return $this;
+    }
+
+    public function getUserMobile(): ?int
+    {
+        return $this->user_mobile;
+    }
+
+    public function setUserMobile(int $user_mobile): self
+    {
+        $this->user_mobile = $user_mobile;
+
+        return $this;
+    }
+
+    public function getUserPassportNumber(): ?string
+    {
+        return $this->user_passport_number;
+    }
+
+    public function setUserPassportNumber(string $user_passport_number): self
+    {
+        $this->user_passport_number = $user_passport_number;
+
+        return $this;
+    }
+
+    public function getUserPassportCountry(): ?string
+    {
+        return $this->user_passport_country;
+    }
+
+    public function setUserPassportCountry(string $user_passport_country): self
+    {
+        $this->user_passport_country = $user_passport_country;
+
+        return $this;
+    }
+
+    public function getUserPassportDate(): ?\DateTimeInterface
+    {
+        return $this->user_passport_date;
+    }
+
+    public function setUserPassportDate(\DateTimeInterface $user_passport_date): self
+    {
+        $this->user_passport_date = $user_passport_date;
+
+        return $this;
+    }
+
+    public function getUserPhoto(): ?string
+    {
+        return $this->user_photo;
+    }
+
+    public function setUserPhoto(string $user_photo): self
+    {
+        $this->user_photo = $user_photo;
+
+        return $this;
+    }
+
+    public function getUserSelectComment(): ?string
+    {
+        return $this->user_select_comment;
+    }
+
+    public function setUserSelectComment(string $user_select_comment): self
+    {
+        $this->user_select_comment = $user_select_comment;
+
         return $this;
     }
 }
